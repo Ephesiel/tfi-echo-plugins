396805099e31beb0d7fd38b23144dba294461e33
Ephesiel <ephesiel@gmail.com>
2020-09-03 12:29:04 +0200

    Correction of an error of upload folder

--- constants.php
+++ constants.php
@@ -10,7 +10,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'ECHO_PATH', plugin_dir_path( __FILE__ ) );
 define( 'ECHO_TABLE', 'echo_datas' );
 
-$upload_dir = wp_upload_dir();
+/**
+ * Call wp_get_upload_dir instead of wp_upload_dir.
+ * It's because we don't want to create the folder, www user doesn't have the permission to do that on the server and it can return an error.
+ */
+$upload_dir = wp_get_upload_dir();
 
 if ( $upload_dir['error'] === false ) {
 	define( 'ECHO_CAMPAIN_FOLDER_DIR', $upload_dir['basedir'] . '/echo_files/' );
